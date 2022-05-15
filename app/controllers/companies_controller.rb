5d25f809ee427f17bb34458bb239c5a3bc3d688c
e-seun <seun.eniolorunda@kellogg.northwestern.edu>
2022-05-15 02:12:51 +0000

    class work updates

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,6 +6,24 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    @contacts = Contacts.findy_by({"company_id" => @company["id"]})
+  end
+
+  def index
+  @companies = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find_by({"id" => params["id"]})
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+    @post.save
+
+    redirect_to "/posts"
   end
 
 end
